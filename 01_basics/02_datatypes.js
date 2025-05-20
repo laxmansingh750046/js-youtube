e5fd85f22f05a418480056b037f77b8b91b94f6e
laxman(CS_MONKS) singh <123521104+laxmansingh750046@users.noreply.github.com>
2025-05-20 17:46:27 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello") // we r using nodejs not browser
+let name="laxman"
+let age=12
+let isLoggedIn=true
+//number 2 to power 53
+// bigint
+//string => ""
+//boolean = true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+//object 
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
