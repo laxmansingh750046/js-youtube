557cc6d7d1728622432b7dcaa84881db27bc33d7
laxman(CS_MONKS) singh <123521104+laxmansingh750046@users.noreply.github.com>
2025-05-21 00:54:13 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score1= "33",score2="33abc"
+console.log(typeof score1);
+console.log(typeof(score1));
+
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)  
+/*
+ -Number()
+  "33abc" to Nan ( not a number)
+  null to 0
+  Undefined to Nan
+  true/false to 1/0
+*/
+console.log(typeof(valueInNumber1));
+console.log(typeof(valueInNumber2));
+console.log(valueInNumber2)   //NaN = ( not a number)
+
+
+let isLoggedIn = -1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+/*
+   0, emptystring => false
+   any negative or positive number => true
+   not empty string => true
+*/
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
