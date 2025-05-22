b31c3adceb22acd054ba70f521bb00aa2c4c95df
laxman(CS_MONKS) singh <123521104+laxmansingh750046@users.noreply.github.com>
2025-05-22 03:32:55 +0000

    math_and_num

--- 01_basics/07_nums_and_math.js
+++ 01_basics/07_nums_and_math.js
@@ -27,3 +27,34 @@ console.log(maxval);   //1.7976931348623157e+308
 console.log(Number.MIN_VALUE) //5e-324
 console.log(Number.MAX_SAFE_INTEGER) //9007199254740991
 console.log(Number.MIN_SAFE_INTEGER) //-9007199254740991
+
+
+//++++++++++ MATHS +++++++++++++++++++++++++++++++++++
+console.log("++++++++++++++++ Maths ++++++++++++++++")
+console.log(Math)
+console.log(Math.abs(-32)) //The absolute value (or modulus) | x | of a real number x is the non-negative value of x without regard to its sign
+console.table([Math.round(4.4), Math.round(4.5), Math.round(4.6)]) //The value is rounded to the nearest integer.
+/*┌─────────┬────────┐
+│ (index) │ Values │
+├─────────┼────────┤
+│ 0       │ 4      │
+│ 1       │ 5      │
+│ 2       │ 5      │
+└─────────┴────────┘ */
+
+console.log(Math.ceil(4.1)) // 5
+console.log(Math.floor(4.9)) //4
+console.log(Math.sqrt(49)) // 7
+
+console.log(Math.min(5,2,6,7)) //2   return minimum value from given arguments
+console.log(Math.min());             // returns Infinity (no arguments)
+
+console.log(Math.max(4,3,6,11,2,21))  //21  return max value from arguments
+console.log(Math.max())        // return -Infinity
+
+console.log(Math.random())  // alway return random values between 0 and 1
+
+// if u want value between specific range  
+const minr = 10, maxr = 20
+// Math.random()*(maxr-minr+1)+minr  // use floor to get integers
+console.log(Math.floor(Math.random()*(maxr-minr+1))+minr)
