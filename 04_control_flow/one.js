f67ae12fb7a848d0158f1998d5b4f29de79c0278
laxman(CS_MONKS) singh <123521104+laxmansingh750046@users.noreply.github.com>
2025-05-24 07:24:17 +0000

    switch, nullish ,ternary

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// switch(key){
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+// if(condition){}
+
+//Falsy values = false, "", 0, 0n(bigint), null, undefined, NaN(not a number)
+//Truthy values = "0", "false", "  ", [], {} , function(){} ( empty function)
+
+// in case of array never check like this ' if(array){};
+// if(Array.lenghth === 0) console.log("Array is empty ");
+
+// if(Object.keys(emptyObject).length === 0){}   // .keys will return array of keys
+
+// Nullish Coalescing Operator(??): null undefined
+    // this operator is just made to check if null or undefined value is comming it will insert other value to variable
+
+// let var1 = null??undefined??12;
+// console.log(var1);
+
+// var1 = 1??12;
+// console.log(var1);
+
+
+// Ternary operator  : condition ? true: false;
