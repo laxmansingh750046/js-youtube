4810051aaf8db4c32f2a6c4853c7df6224b1c024
laxman(CS_MONKS) singh <123521104+laxmansingh750046@users.noreply.github.com>
2025-05-24 08:01:23 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,26 @@
+// for
+// for (let i = 0; i < 5; i++) {
+//   console.log("The number is " + i);
+// }
+
+// let myarray = ["flash", "batman", "superman"];
+
+// for (let i = 0; i < myarray.length; i++) {
+//   console.log(myarray[i]);
+// }
+
+// let myarray = ["flash", "batman", "superman", "wonderwoman", "aquaman"];
+// let i = 0;
+
+// while (i < myarray.length) {
+//   console.log(myarray[i]);
+//   i++;
+// }
+
+// let myarray = ["flash", "batman", "superman", "wonderwoman", "aquaman"];
+// let i = 0;
+
+// do {
+//   console.log(myarray[i]);
+//   i++;
+// } while (i < myarray.length);
