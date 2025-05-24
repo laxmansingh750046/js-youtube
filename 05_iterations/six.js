0a87720a39507d2f3c39ae1e071fe746f2f5d576
laxman(CS_MONKS) singh <123521104+laxmansingh750046@users.noreply.github.com>
2025-05-24 10:28:58 +0000

    map

---
+++ 05_iterations/six.js
@@ -0,0 +1,23 @@
+const nums = [1,2,3,4,5,6,7,8,9,10]
+
+// const numPlus10 = nums.map((num)=>num+10);
+// console.log(numPlus10);
+
+
+// const newNums = nums
+//                  .map((num)=>num+10)
+//                  .map((x)=>x+1)
+// console.log(newNums);
+
+
+// const newNums = nums
+//                  .map((num)=>num+10)
+//                  .length
+// console.log(newNums);  //10
+
+
+// const newNums = nums
+//                  .map((num)=>num*10)
+//                  .map((num)=>num+10)
+//                  .filter((num)=>num<60);
+// console.log(newNums);  //[ 20, 30, 40, 50 ]
