11e0a645085468830b8effb312431e2b2ec7c01f
laxman(CS_MONKS) singh <123521104+laxmansingh750046@users.noreply.github.com>
2025-05-28 01:44:10 +0000

    promise_async/await_fetch

---
+++ 09_advance_one/promise.js
@@ -0,0 +1,94 @@
+const promiseOne = new Promise(function(resolve,reject){
+    //Do an async task,  db calls, cryptograhy, network
+    setTimeout(() => {
+        console.log("promise1 async task completed");
+        resolve();
+    }, 1000);
+});
+
+promiseOne.then(function(){
+    console.log('Promise 1 consumed');
+})
+
+
+
+//   promiseTwo
+new Promise(function(resolve,reject){
+    setTimeout(() => {
+        console.log("promise 2Resolved");
+        resolve();
+    }, 1000);
+})
+.then(function(){
+    console.log("promise 2 consumed");
+});
+
+
+
+const promiseThree = new Promise(function(res,rej){
+    setTimeout(function(){
+        res({username:"laxman",email:"dmon@mx.com"});
+    }, 1000);
+});
+promiseThree.then((user)=>{
+    console.log("promise 3 user: ",user);
+});
+
+
+
+// promise4
+new Promise((res,rej)=>{
+    setTimeout(() => {
+        rej("SOME THING WENT WRONG");
+    }, (1000));
+})
+.then((e)=>{console.log(e);})
+.catch((e)=>{console.log("promise 4: ",e);})
+.finally(()=>{console.log('Promise 4 either resolved or rejected')});
+
+
+
+const promiseFive = new Promise((res,rej)=>{
+    setTimeout(() => {
+        rej("Error with js");
+    }, 1000);
+});
+
+async function consumedPromise(){
+    try{
+        const response= await promiseFive;
+        return response;
+    }
+    catch(error){
+        console.log("promise 5",error);
+    }
+}
+
+consumedPromise();
+
+
+
+async function getAllUsers(){
+    try{
+        const respons = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await respons.json();
+        console.log("promise 6 ",typeof data);
+    }
+    catch(error){
+        console.log("Error: ",error);
+    }
+}
+getAllUsers();
+
+
+
+fetch('https://api.github.com/users/laxmansingh750046')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log("promise 7: ", data);
+})
+.catch((error)=>{
+    console.error(error);
+});
